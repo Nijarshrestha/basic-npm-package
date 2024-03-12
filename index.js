982e7b7706e048c817dc62e7c89740e386ff596e
Nijarshrestha <shresthanijar@gmail.com>
2024-03-12 16:31:11 +0545

    chore: main, added new function and install prettier and linting

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 export function helloWorld() {
-  return 'Hello World from NPM and use something';
+    return 'Hello World from NPM and use something'
+}
+
+export function getSumOfArray(array) {
+    return array.reduce((acc, sum) => acc + sum, 0)
 }
